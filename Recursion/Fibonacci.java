dfe12bf9d6d13b07928a8d7b672c3b78d7d6a94e
SR15KT <sr15kt.dev@gmail.com>
2025-06-13 11:25:29 +0530

    Update Fibonacci.java

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -1,4 +1,4 @@
-//Recursion
+//Nth element in a fibonacci series
 public class FibonacciRecursion {
     public static void main(String[] args) {
         int ans= fibonacci(4);
@@ -11,3 +11,24 @@ public class FibonacciRecursion {
         return fibonacci(n-1)+fibonacci(n-2);
     }
 }
+
+
+
+// Fibonacci series
+import java.util.Scanner;
+
+public class FibonacciRecursion {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+        int n= scanner.nextInt();
+        for(int i=0;i<n;i++){
+        System.out.print(fibonacci(i)+" " );
+    }}
+     static int fibonacci(int n){
+        if(n<2){
+            return n;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
+
